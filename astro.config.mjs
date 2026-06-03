801b1a72d52d17c62c017152f080aa4e204059f9
stearnsbq <stearnsbq@vcu.edu>
2026-06-02 22:52:28 -0400

    big setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import wasm from 'vite-plugin-wasm'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+      vite: {
+    plugins: [tailwindcss(), wasm()],
+    optimizeDeps: { exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'] }
+  },
+});

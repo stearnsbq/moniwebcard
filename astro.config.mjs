88125607a44715d2da7f9bc61dce4131499a4c98
stearnsbq <stearnsbq@vcu.edu>
2026-06-02 22:55:10 -0400

    root

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import wasm from 'vite-plugin-wasm'
 
 // https://astro.build/config
 export default defineConfig({
+   site: 'https://stearnsbq.github.io',
       vite: {
     plugins: [tailwindcss(), wasm()],
     optimizeDeps: { exclude: ['@ffmpeg/ffmpeg', '@ffmpeg/util'] }
